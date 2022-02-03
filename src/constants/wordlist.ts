32e4b59923a595e359365aa1fac0c4e3ad91c75f
Tanay Amonkar <tanay.a@TRADESTONE.local>
2022-02-03 16:41:13 +0530

    first comms

---
+++ src/constants/wordlist.ts
@@ -0,0 +1,3 @@
+export const WORDS = [
+  'dread','taken','twins','signs','holes','speed','hitch','mulan','shark','crash','senna','fargo','rocky','signs','rango','ronin','frida','shane','glory','mimic','alien','bambi','haven','wings','seven','brave','virus','chaos','dudes','slash','diner','shade','pulse','dolls','scoop','shrek','ponyo','magic','click','nixon','foxes','tears','tenet','timer','shock','spike','vamps','diane','giant','jaded','evita','north','shaft','julia','turbo',
+]
